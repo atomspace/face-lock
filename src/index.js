49edfd8a970ed3b3cc010b6094420c5d4b702985
PyTechMike <technomike04@gmail.com>
2018-03-25 21:52:41 +0300

    Absent added

--- src/index.js
+++ src/index.js
@@ -8,11 +8,18 @@ window.onload = function() {
     tracker.setStepSize(2);
     tracker.setEdgesDensity(0.1);
 
+    var isFace = false;
+
     tracking.track('#video', tracker, { camera: true });
 
     tracker.on('track', function(event) {
       context.clearRect(0, 0, canvas.width, canvas.height);
-
+      if (event.data.length === 0) {
+        if(isFace){
+          isFace = false;
+          console.log('ABSENT');
+        }
+      } else {
       event.data.forEach(function(rect) {
         context.strokeStyle = '#a64ceb';
         context.strokeRect(rect.x, rect.y, rect.width, rect.height);
@@ -20,7 +27,10 @@ window.onload = function() {
         context.fillStyle = "#fff";
         context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
         context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
-        console.log('PRESENT');
+        if(!isFace){
+          isFace = true;
+          console.log('PRESENT');
+        }
       });
       
     });
