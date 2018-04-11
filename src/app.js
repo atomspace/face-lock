4a115ead3576dd774dd9e9064c5cdae21a9db446
PyTechMike <technomike04@gmail.com>
2018-04-11 22:53:51 +0300

    Icon added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Tray } = require('electron')
 const path = require('path')
 const url = require('url')
 
@@ -6,10 +6,14 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 
+
+
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
-
+  win = new BrowserWindow({ width: 800, height: 600})
+  
+  var appIcon = new Tray('src/camera.png')
+  
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
