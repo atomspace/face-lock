36c30536732cd495ea01035e7ae81e1b594873a7
PyTechMike <pytechmike@gmail.com>
2018-05-28 00:42:06 +0300

    Face service impruved (the probability of face recognition increased)

--- src/services/face.service.js
+++ src/services/face.service.js
@@ -2,7 +2,13 @@ require('tracking');
 require('tracking/build/data/face');
 
 let video = document.getElementById('video');
+let canvas = document.getElementById('canvas');
 let tracker = new tracking.ObjectTracker('face');
+let context = canvas.getContext('2d');
+
+tracker.setInitialScale(4);
+tracker.setStepSize(2);
+tracker.setEdgesDensity(0.1);
 
 let present = false;
 
@@ -13,6 +19,7 @@ module.exports = {
 	onAbsent (callback) {
 		tracker.on('track', function (event) {
 			if (event.data.length === 0 && present) {
+				context.clearRect(0, 0, canvas.width, canvas.height);
 				present = false;
 				callback();
 			}
@@ -22,7 +29,10 @@ module.exports = {
 	onPresent (callback) {
 		tracker.on('track', function (event) {
 			if (event.data.length > 0 && !present) {
-				event.data.forEach(function () {
+				event.data.forEach(function (rect) {
+					context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+					context.fillText(`x: ${rect.x}px`, rect.x + rect.width + 5, rect.y + 11);
+					context.fillText(`y: ${rect.y}px`, rect.x + rect.width + 5, rect.y + 22);
 					present = true;
 					callback();
 				});
